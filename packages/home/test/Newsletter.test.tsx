9802f518b352dcabd07e9d151c0ed00726924e92
Jorge Luis Bermudez <jlube3092@gmail.com>
2022-02-18 16:53:26 -0500

    test: :white_check_mark: added aditional test to Newsletter component

--- packages/home/test/Newsletter.test.tsx
+++ packages/home/test/Newsletter.test.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import Newsletter from '../src/components/Newsletter';
 // import handleSubmit from '../src/components/Newsletter'
+
+let container: any = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
 describe('<Newsletter />', () => {
   it('should render complete newsletter subscribe form', () => {
@@ -15,9 +29,27 @@ describe('<Newsletter />', () => {
     ).toBeTruthy();
   });
 
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // it('should return false if email use a ","', () => {
-  //   const initials = handleSubmit("Jhondoe@mail.com");
-  //   expect(initials).toBe(false);
-  // });
+  it('should no call any event without no data in form', async () => {
+    act(() => {
+      render(
+        <Newsletter
+          title="Suscríbete a nuestro newsletter"
+          submit="Suscríbete"
+          onSubmit={(email: string) => {
+            alert(`Subscribing ${email} to the newsletter.`);
+          }}
+        />,
+        container
+      );
+    });
+
+    const validateSpy = jest.fn();
+
+    const button = container.querySelector('[data-testid="store-loading-button"]');
+
+    await act(async () => {
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    });
+    expect(validateSpy.mock.calls.length).toBe(1);
+  });
 });
