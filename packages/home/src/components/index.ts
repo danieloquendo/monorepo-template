d7a7029fd5bcc33748138c76ac7e5dc560041f92
DiegoFavianGarcia <92319857+DiegoFavianGarcia@users.noreply.github.com>
2022-02-18 09:49:35 -0500

    fix: :art: added export for interfaces
    Exported Interfaces for Banner and Slider
    
    Export Props from component

--- packages/home/src/components/index.ts
+++ packages/home/src/components/index.ts
@@ -1,5 +1,6 @@
 import HomeExample from './HomeExample';
 import HomeTest from './HomeTest';
 import BannerWithSlider from './BannerWidthSlider';
+import { BannerData, SilderData, BannerWithSliderProps } from './BannerWidthSlider/Container';
 
-export { HomeExample, HomeTest, BannerWithSlider };
+export { HomeExample, HomeTest, BannerWithSlider, BannerData, SilderData, BannerWithSliderProps };
